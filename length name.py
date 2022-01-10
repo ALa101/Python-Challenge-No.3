14bdf6021362a837047228bc4bf4d936bd830b0a
BakrAldubai <eng.bakraldubai@gmail.com>
2022-01-10 15:15:59 +0300

    Python Challenge No.3

---
+++ length name.py
@@ -0,0 +1,10 @@
+"""
+                    2.length first name
+Ask the user to enter their first name
+and then display the length of their name.
+ upload program file to GitHub and past the link.
+
+"""
+
+firstName = input("Enter First name :")
+print("the length of first name is : (" + str(len(firstName)) + ")")
